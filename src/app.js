f46f91308c0d9641f0986ebc5221e5b9b2e73719
Eidrian2005 <eserrano@fwdcostarica.com>
2024-08-16 14:06:39 -0600

    ejercicio 5

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { calculadora } from "./index.js";
+
+let c = new calculadora(6,3)
+
+console.log(c.sumar());
+console.log(c.restar());
+console.log(c.dividir());
+console.log(c.multiplicar());
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 // Inserte el código aquí
 
+class calculadora {
+    constructor(numero1, numero2) {
+        this.numero1 = numero1
+        this.numero2 = numero2
+    }
+
+    sumar(){
+        return this.numero1 + this.numero2
+    }
+
+    restar(){
+        return this.numero1 - this.numero2
+    }
+
+
+    multiplicar(){
+       return this.numero1 * this.numero2
+    }
+
+    dividir(){
+        return this.numero1 / this.numero2
+    }
+}
+
+export{
+    calculadora
+}
